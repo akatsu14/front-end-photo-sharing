60de8f00d17f04e79b42ed3a604fa6c735e0e056
akatsu14 <luong13166@gmail.com>
2024-05-17 00:31:03 +0700

    online

--- src/utils/socketComment.js
+++ src/utils/socketComment.js
@@ -4,10 +4,10 @@ import { io } from "socket.io-client";
 // export const socketChat = io.connect("http://localhost:3000");
 // export const socketComment = io.connect("http://localhost:8080");
 
-export const BaseUrl = "http://26.208.114.46:8081";
-export const socketChat = io.connect("http://26.208.114.46:3000");
-export const socketComment = io.connect("http://26.208.114.46:8080");
+// export const BaseUrl = "http://26.208.114.46:8081";
+// export const socketChat = io.connect("http://26.208.114.46:3000");
+// export const socketComment = io.connect("http://26.208.114.46:8080");
 
-// export const BaseUrl = "https://dxfz3n-8081.csb.app";
-// export const socket = io.connect("https://dxfz3n-3000.csb.app");
-// export const socketComment = io.connect("https://dxfz3n-8080.csb.app");
+export const BaseUrl = "https://dxfz3n-8081.csb.app";
+export const socket = io.connect("https://dxfz3n-3000.csb.app");
+export const socketComment = io.connect("https://dxfz3n-8080.csb.app");
